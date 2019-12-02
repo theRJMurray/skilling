04f5e888fa53907c4504038e4507e133ab118866
therjmurray <therjmurray@gmail.com>
2019-12-02 18:43:56 -0500

    moved Skill to its own file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import Skill from "./components/Skill";
 
 const App = () => {
   return (
@@ -6,31 +7,6 @@ const App = () => {
       <Skill skillName={"Woodcutting"} skillAction={"Chop Wood"} />
       <Skill skillName={"Mining"} skillAction={"Mine Ore"} />
       <Skill skillName={"Fishing"} skillAction={"Catch Fish"} />
-    </div>
-  );
-};
-
-const Skill = props => {
-  const [exp, setExp] = useState(0);
-  const [level, setLevel] = useState(1);
-
-  const levelMarks = [5, 15, 30, 50, 100];
-
-  const gainExp = () => {
-    setExp(exp + 1);
-    if (levelMarks.includes(exp + 1)) {
-      setLevel(level + 1);
-    }
-  };
-
-  return (
-    <div>
-      <h1>{props.skillName}</h1>
-      <h2>Level: {level} </h2>
-      <h3>Experience: {exp}</h3>
-      <div>
-        <button onClick={gainExp}>{props.skillAction}</button>
-      </div>
     </div>
   );
 };
---
+++ src/components/Skill.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Skill = props => {
+    const [exp, setExp] = useState(0);
+    const [level, setLevel] = useState(1);
+
+    const levelMarks = [5, 15, 30, 50, 100];
+
+    const gainExp = () => {
+        setExp(exp + 1);
+        if (levelMarks.includes(exp + 1)) {
+            setLevel(level + 1);
+        }
+    };
+
+    return (
+        <div>
+            <h1>{props.skillName}</h1>
+            <h2>Level: {level} </h2>
+            <h3>Experience: {exp}</h3>
+            <div>
+                <button onClick={gainExp}>{props.skillAction}</button>
+            </div>
+        </div>
+    );
+};
+
+export default Skill;
