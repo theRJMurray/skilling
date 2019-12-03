3bab7f7ba84c3c7ec46f7427fb8007b8892bccb8
therjmurray <therjmurray@gmail.com>
2019-12-02 21:14:06 -0500

    started inventory feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Skill from "./components/Skill";
+import './css/Inventory.css'
 
 const App = () => {
-  return (
-    <div>
-      <Skill skillName={"Woodcutting"} skillAction={"Chop Wood"} />
-      <Skill skillName={"Kite Flying"} skillAction={"Fly Kite"} />
-      <Skill skillName={"Parkour"} skillAction={"Climb and Jump"} />
-      <Skill skillName={"Coding"} skillAction={"Roll Face on Keyboard"} />
-      <Skill skillName={"Wizardy"} skillAction={"Cast Spells"} />
-    </div>
-  );
+	return (
+		<div style={{ display: 'flex' }}>
+			<div>
+				<Skill skillName={"Woodcutting"} skillAction={"Chop Wood"} />
+				<Skill skillName={"Kite Flying"} skillAction={"Fly Kite"} />
+				<Skill skillName={"Parkour"} skillAction={"Climb and Jump"} />
+				<Skill skillName={"Coding"} skillAction={"Roll Face on Keyboard"} />
+				<Skill skillName={"Wizardy"} skillAction={"Cast Spells"} />
+			</div>
+			<div>
+				<Inventory />
+			</div>
+
+		</div>
+	);
 };
 
 export default App;
+
+const Inventory = () => {
+	let starterKit = [
+		{ id: 0, name: 'shovel', quantity: 1 },
+		{ id: 1, name: 'pickle', quantity: 4 }
+	]
+	const [inventory, setInventory] = useState(starterKit)
+
+	return (
+		<div className="inventoryContainer">
+			{inventory.map((o) =>
+				<div>
+					<p>{o.name}</p>
+					<p>{o.quantity}</p>
+				</div>)}
+		</div>
+	);
+}
