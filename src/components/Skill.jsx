d95a9abac678339ca69d0ae208dc3b2631a1f0ee
therjmurray <therjmurray@gmail.com>
2019-12-24 22:21:31 -0500

    wierdness

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
-import '../css/Skill.css'
+import React, { useState } from "react";
+import "../css/Skill.css";
 
 const Skill = props => {
-	const [visible, setVisible] = useState(false);
   const [visible, setVisible] = useState(props.visibility || false);
 
-	const clickShow = () => {
-		visible ? setVisible(false) : setVisible(true)
-	}
   const clickShow = () => {
     visible ? setVisible(false) : setVisible(true);
   };
 
-	return (
-		<div className="skillContainer">
-			<div className="skillTitleBox">
-				<h1 className="skillTitle" onClick={clickShow}> {props.skillName} </h1>
-			</div>
-			<>
-				<div className="statsBox">
-					<h2 className="levelInfo">Level: {props.skill.level} </h2>
-					<h3 className="expInfo">Experience: {props.skill.exp} </h3>
-					<div className="buttonContainer">
-						<button className="btn btn-primary" onClick={props.takeAction}> {props.skillAction} </button>
-					</div>
-				</div>
-			</>
-		</div>
-	);
+  return (
+    <div className="skillContainer">
+      <div className="skillTitleBox">
+        <h1 className="skillTitle" onClick={clickShow}>
+          {props.skillName}
+        </h1>
+      </div>
       {visible && (
         <>
           <div className="statsBox">
@@ -41,6 +28,8 @@ const Skill = props => {
           </div>
         </>
       )}
+    </div>
+  );
 };
 
 export default Skill;
