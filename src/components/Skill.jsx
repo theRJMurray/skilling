5fc4f838ba692aaf25249420e2cf604dfcb19ce2
therjmurray <therjmurray@gmail.com>
2019-12-24 22:19:29 -0500

    created drop down capability
    to assist with smaller screens
    set as default for all skills except woodcutting

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,10 +3,14 @@ import '../css/Skill.css'
 
 const Skill = props => {
 	const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(props.visibility || false);
 
 	const clickShow = () => {
 		visible ? setVisible(false) : setVisible(true)
 	}
+  const clickShow = () => {
+    visible ? setVisible(false) : setVisible(true);
+  };
 
 	return (
 		<div className="skillContainer">
@@ -24,6 +28,19 @@ const Skill = props => {
 			</>
 		</div>
 	);
+      {visible && (
+        <>
+          <div className="statsBox">
+            <h2 className="levelInfo">Level: {props.skill.level} </h2>
+            <h3 className="expInfo">Experience: {props.skill.exp} </h3>
+            <div className="buttonContainer">
+              <button className="btn btn-primary" onClick={props.takeAction}>
+                {props.skillAction}
+              </button>
+            </div>
+          </div>
+        </>
+      )}
 };
 
 export default Skill;
